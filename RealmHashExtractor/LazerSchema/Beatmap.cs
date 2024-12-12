0166cdfec72bf0497e42290f115ebaf71009fec4
taoky <me@taoky.moe>
2024-12-12 14:31:58 +0800

    Update schema version

--- RealmHashExtractor/LazerSchema/Beatmap.cs
+++ RealmHashExtractor/LazerSchema/Beatmap.cs
@@ -44,34 +44,6 @@ namespace CollectionDowngrader.LazerSchema
 
         public bool Hidden { get; set; }
 
-        public double AudioLeadIn { get; set; }
-
-        public float StackLeniency { get; set; } = 0.7f;
-
-        public bool SpecialStyle { get; set; }
-
-        public bool LetterboxInBreaks { get; set; }
-
-        public bool WidescreenStoryboard { get; set; }
-
-        public bool EpilepsyWarning { get; set; }
-
-        public bool SamplesMatchPlaybackRate { get; set; }
-
-        public DateTimeOffset? LastPlayed { get; set; }
-
-        public double DistanceSpacing { get; set; }
-
-        public int BeatDivisor { get; set; }
-
-        public int GridSize { get; set; }
-
-        public double TimelineZoom { get; set; }
-
-        public double? EditorTimestamp { get; set; }
-
-        public int CountdownOffset { get; set; }
-
         // Author kabii
         public override bool Equals(object? obj)
         {
--- RealmHashExtractor/Program.cs
+++ RealmHashExtractor/Program.cs
@@ -22,7 +22,7 @@ if (!File.Exists(realmFile))
     return;
 }
 
-const int LazerSchemaVersion = 43;
+const int LazerSchemaVersion = 44;
 RealmConfiguration config = new(realmFile)
 {
     IsReadOnly = true,
